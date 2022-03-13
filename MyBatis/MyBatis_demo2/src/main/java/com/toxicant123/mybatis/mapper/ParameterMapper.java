4065cea7ffd45ad73fa1fd18479502b4b25e46a0
toxicant123 <13655847550@163.com>
2022-03-13 17:41:17 +0800

    0.0.11 MyBatis

--- MyBatis/MyBatis_demo2/src/main/java/com/toxicant123/mybatis/mapper/ParameterMapper.java
+++ MyBatis/MyBatis_demo2/src/main/java/com/toxicant123/mybatis/mapper/ParameterMapper.java
@@ -2,8 +2,10 @@ package com.toxicant123.mybatis.mapper;
 
 
 import com.toxicant123.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author toxicant123
@@ -32,4 +34,26 @@ public interface ParameterMapper {
      * @return
      */
     User checkLogin(String name, String password);
+
+    /**
+     * 验证登录（参数为map）
+     * @param map
+     * @return
+     */
+    User checkLoginByMap(Map<String,Object> map);
+
+    /**
+     * 添加用户信息
+     * @param user
+     * @return
+     */
+    int insertUser(User user);
+
+    /**
+     * 验证登录（使用@Param）
+     * @param username
+     * @param password
+     * @return
+     */
+    User checkLoginByParam(@Param("username")String username, @Param("password")String password);
 }
--- MyBatis/MyBatis_demo2/src/test/java/com/toxicant123/mybatis/test/ParameterMapperTest.java
+++ MyBatis/MyBatis_demo2/src/test/java/com/toxicant123/mybatis/test/ParameterMapperTest.java
@@ -7,7 +7,9 @@ import com.toxicant123.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author toxicant123
@@ -28,6 +30,15 @@ public class ParameterMapperTest {
      * 此时MyBatis会将这些参数放在一个map集合中，以两种方式进行存储
      * a> 以arg0,arg1...为键，以参数为值
      * b> 以param1,param2...为键，以参数为值
+     * 因此只需要通过#{}和${}以键的方式访问值即可，但是需要注意${}的单引号问题
+     * 3. 若mapper接口方法的参数有多个时，可以手动将这些参数放在一个map中存储
+     * 可以通过${key}和#{key}获取参数值，但是需要注意${}的单引号问题
+     * 4. mapper接口方法的参数是实体类类型的参数
+     * 可以通过${属性名}和#{属性名}获取参数值，但是需要注意${}的单引号问题
+     * 5. 使用@Param注解命名参数
+     * 此时MyBatis会将这些参数放在一个map集合中，以两种方式进行存储
+     * a> 以@Param注解的值为键，以参数为值
+     * b> 以param1，param2...为键，以参数为值
      * 因此只需要通过#{}和${}以键的方式访问值即可，但是需要注意${}的单引号问题
      */
 
@@ -52,6 +63,33 @@ public class ParameterMapperTest {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
         User user = mapper.checkLogin("admin","123");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testChecLoginByMap() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        Map<String,Object> map = new HashMap<>();
+        map.put("username","admin");
+        map.put("password",123);
+        User user = mapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        int result = mapper.insertUser(new User(null, "lihua", "123456", 23, "f", "lihua@163.com"));
+        System.out.println(result);
+    }
+
+    @Test
+    public void testChecLoginByParam() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+        User user = mapper.checkLoginByParam("admin","123");
         System.out.println(user);
     }
 }
