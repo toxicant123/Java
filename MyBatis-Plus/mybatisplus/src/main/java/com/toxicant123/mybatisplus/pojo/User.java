4b978318516e516cf5fa6f78a88b2e35584f9ded
toxicant123 <13655847550@163.com>
2022-03-18 09:54:13 +0800

    0.1.8 MyBatis-Plus

--- MyBatis-Plus/mybatisplus/src/main/java/com/toxicant123/mybatisplus/pojo/User.java
+++ MyBatis-Plus/mybatisplus/src/main/java/com/toxicant123/mybatisplus/pojo/User.java
@@ -1,5 +1,8 @@
 package com.toxicant123.mybatisplus.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -10,14 +13,26 @@ import lombok.Data;
  * @create 2022-03-17 10:45
  */
 @Data
-@TableName
+//设置实体类所对应的表名
+@TableName("t_user")
 public class User {
-
+    /**
+     * 将属性所对应的字段指定为主键
+     *  @TableId 注解的value属性用于指定主键的字段
+     *  @TableId 注解的type属性设置主键生成策略
+     *  @TableId(value = "uid", type = IdType.AUTO)
+     */
+    @TableId("id")
     private Long id;
 
+    //指定属性所对应的字段名
+    @TableField("name")
     private String name;
 
     private Integer age;
 
     private String email;
+
+    @TableLogic
+    private Integer is_deleted;
 }
