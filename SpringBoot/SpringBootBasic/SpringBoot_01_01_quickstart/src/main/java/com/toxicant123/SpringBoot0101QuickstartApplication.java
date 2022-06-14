d32d69affae61a9376cbf6f8765c1057e870c9c9
toxicant123 <13655847550@163.com>
2022-06-14 08:58:45 +0800

    0.5.3 SpringBoot

---
+++ SpringBoot/SpringBootBasic/SpringBoot_01_01_quickstart/src/main/java/com/toxicant123/SpringBoot0101QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.toxicant123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot0101QuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot0101QuickstartApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/SpringBootBasic/SpringBoot_01_01_quickstart/src/test/java/com/toxicant123/SpringBoot0101QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toxicant123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot0101QuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
