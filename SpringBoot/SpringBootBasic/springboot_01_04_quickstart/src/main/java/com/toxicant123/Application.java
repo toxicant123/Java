f439626c027fd3908aa7e9951d6de498e9f9904f
toxicant123 <13655847550@163.com>
2022-06-17 08:43:26 +0800

    0.5.7 SpringBoot

---
+++ SpringBoot/SpringBootBasic/springboot_01_04_quickstart/src/main/java/com/toxicant123/Application.java
@@ -0,0 +1,17 @@
+package com.toxicant123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2022-06-17 8:40
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ SpringBoot/SpringBootBasic/springboot_01_04_quickstart/src/main/java/com/toxicant123/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.toxicant123.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest模式
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2022-06-14 8:46
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String getById(){
+        System.out.println("springboot is running...4");
+        return "springboot is running...4";
+    }
+}
