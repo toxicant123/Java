0f76eb5755878bf72c39eb49b420a658f384cd61
toxicant123 <13655847550@163.com>
2022-07-01 22:51:01 +0800

    0.5.31 SpringBoot

--- SpringBoot/SpringBootBasic/springboot_02_base_configuration/src/main/java/com/toxicant123/controller/BookController.java
+++ SpringBoot/SpringBootBasic/springboot_02_base_configuration/src/main/java/com/toxicant123/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.toxicant123.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    @Value("${country}")
+    private String country1;
+
+    @Value("${user1.name}")
+    private String name1;
+
+    @Value("${likes[1]}")
+    private String hobby;
 
     @GetMapping
     public String getById(){
         System.out.println("springboot is running");
+        System.out.println("country1: " + country1);
+        System.out.println("name: " + name1);
+        System.out.println("hobby: " + hobby);
         return "springboot is running";
     }
 }
