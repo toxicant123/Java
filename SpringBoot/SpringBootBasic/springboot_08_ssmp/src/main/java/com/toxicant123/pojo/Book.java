8eb08730dda46e2312ece645fe89c46a3d3919bd
toxicant123 <13655847550@163.com>
2022-07-12 23:03:44 +0800

    0.5.54 SpringBoot

---
+++ SpringBoot/SpringBootBasic/springboot_08_ssmp/src/main/java/com/toxicant123/pojo/Book.java
@@ -0,0 +1,17 @@
+package com.toxicant123.pojo;
+
+import lombok.Data;
+
+/**
+ * @author toxicant123
+ * @version 1.0
+ * @Description
+ * @create 2022-07-12 22:52
+ */
+@Data
+public class Book {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+}
