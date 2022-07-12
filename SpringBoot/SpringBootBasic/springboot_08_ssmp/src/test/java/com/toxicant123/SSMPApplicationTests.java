ae7d9d055b9c3812856afa6ca71cbbbd48ae3fec
toxicant123 <13655847550@163.com>
2022-07-13 01:38:31 +0800

    0.5.56 SpringBoot

--- SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
+++ SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
@@ -1,6 +1,7 @@
 package com.toxicant123;
 
 import com.toxicant123.dao.BookDao;
+import com.toxicant123.pojo.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +11,18 @@ class SSMPApplicationTests {
     @Autowired
     private BookDao bookDao;
     @Test
-    public void test1() {
-        bookDao.selectOne()
+    public void testGetById() {
+        Book book = bookDao.selectById(1);
+        System.out.println(book);
+    }
+    
+    @Test
+    public void testSave(){
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookDao.insert(book);
     }
 
 }
