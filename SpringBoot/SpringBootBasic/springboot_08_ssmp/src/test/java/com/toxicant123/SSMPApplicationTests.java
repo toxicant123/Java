785cfb2af6d08576f2c0f22a3054af1b3c5adba0
toxicant123 <13655847550@163.com>
2022-07-13 22:04:25 +0800

    0.5.57 SpringBoot

--- SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
+++ SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
@@ -1,10 +1,13 @@
 package com.toxicant123;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.toxicant123.dao.BookDao;
 import com.toxicant123.pojo.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SSMPApplicationTests {
@@ -24,5 +27,30 @@ class SSMPApplicationTests {
         book.setDescription("测试数据123");
         bookDao.insert(book);
     }
+    
+    @Test
+    public void testUpdate(){
+        QueryWrapper<Book> wrap = new QueryWrapper<>();
+        wrap.select("id").orderByDesc("id");
+        List<Book> books = bookDao.selectList(wrap);
+        Book book = new Book();
+        book.setId(books.get(0).getId());
+        book.setName("水浒传");
+        book.setType("古代小说");
+        book.setDescription("108的传奇");
+    }
 
+    @Test
+    public void testDelete(){
+        QueryWrapper<Book> wrap = new QueryWrapper<>();
+        wrap.select("id").orderByDesc("id");
+        List<Book> books = bookDao.selectList(wrap);
+        bookDao.deleteById(books.get(0).getId());
+    }
+    
+    @Test
+    public void testGetAll(){
+        List<Book> books = bookDao.selectList(null);
+        System.out.println(books);
+    }
 }
