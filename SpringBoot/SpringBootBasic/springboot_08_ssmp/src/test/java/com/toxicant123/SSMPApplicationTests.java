1df737e0508b8f94389df082d49c34a4c81462e8
toxicant123 <13655847550@163.com>
2022-07-15 02:22:59 +0800

    0.5.61 SpringBoot

--- SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
+++ SpringBoot/SpringBootBasic/springboot_08_ssmp/src/test/java/com/toxicant123/SSMPApplicationTests.java
@@ -1,5 +1,6 @@
 package com.toxicant123;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.toxicant123.dao.BookDao;
@@ -14,23 +15,24 @@ import java.util.List;
 class SSMPApplicationTests {
     @Autowired
     private BookDao bookDao;
+
     @Test
     public void testGetById() {
         Book book = bookDao.selectById(1);
         System.out.println(book);
     }
-    
+
     @Test
-    public void testSave(){
+    public void testSave() {
         Book book = new Book();
         book.setType("测试数据123");
         book.setName("测试数据123");
         book.setDescription("测试数据123");
         bookDao.insert(book);
     }
-    
+
     @Test
-    public void testUpdate(){
+    public void testUpdate() {
         QueryWrapper<Book> wrap = new QueryWrapper<>();
         wrap.select("id").orderByDesc("id");
         List<Book> books = bookDao.selectList(wrap);
@@ -42,23 +44,23 @@ class SSMPApplicationTests {
     }
 
     @Test
-    public void testDelete(){
+    public void testDelete() {
         QueryWrapper<Book> wrap = new QueryWrapper<>();
         wrap.select("id").orderByDesc("id");
         List<Book> books = bookDao.selectList(wrap);
         bookDao.deleteById(books.get(0).getId());
     }
-    
+
     @Test
-    public void testGetAll(){
+    public void testGetAll() {
         List<Book> books = bookDao.selectList(null);
         System.out.println(books);
     }
-    
+
     @Test
-    public void testGetPage(){
+    public void testGetPage() {
         Page<Book> page = new Page<>(1, 5);
-        bookDao.selectPage(page,null);
+        bookDao.selectPage(page, null);
         System.out.println(page.getCurrent());
         System.out.println(page.getSize());
         System.out.println(page.getTotal());
@@ -67,8 +69,44 @@ class SSMPApplicationTests {
     }
 
     @Test
-    public void testGetPage2(){
+    public void testGetPage2() {
         Page<Book> page = new Page<>(3, 5);
-        bookDao.selectPage(page,null);
+        bookDao.selectPage(page, null);
+    }
+
+    @Test
+    public void testGetBy() {
+        QueryWrapper<Book> wrapper = new QueryWrapper<>();
+        wrapper.like("name", "spring");
+        bookDao.selectList(wrapper);
+    }
+
+    @Test
+    public void testGetBy2(){
+        LambdaQueryWrapper<Book> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(Book::getName,"spring");
+        bookDao.selectList(wrapper);
+    }
+
+    @Test
+    public void testGetBy3(){
+        String name = null;
+        LambdaQueryWrapper<Book> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(name != null,Book::getName,name);
+        bookDao.selectList(wrapper);
+    }
+
+    @Test
+    public void testGetBy4(){
+        QueryWrapper<Book> wrapper = new QueryWrapper<>();
+        wrapper.select("count(*) count");
+        bookDao.selectOne(wrapper);
+    }
+
+    @Test
+    public void testGetBy5(){
+        QueryWrapper<Book> wrapper = new QueryWrapper<>();
+        wrapper.select("max(id) maxOne");
+        bookDao.selectOne(wrapper);
     }
 }
